9b8461124579b6d632fe262bfa6459ce95a458dd
Yaroslav <toasolek@gmail.com>
2022-01-19 15:32:11 +0700

    Initial Commit

---
+++ Cinema2/Cinema2App.swift
@@ -0,0 +1,17 @@
+//
+//  Cinema2App.swift
+//  Cinema2
+//
+//  Created by Yaroslav Derbyshev on 19.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct Cinema2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
